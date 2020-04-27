0fff45a1d1dcc5fd65c0def992c7babe2d12b7a4
m-walker-student <60361266+m-walker-student@users.noreply.github.com>
2020-04-27 13:01:16 -0400

    Add files via upload

--- MMP100/programming/sketch.js
+++ MMP100/programming/sketch.js
@@ -8,6 +8,10 @@ let gVal=0;
 let bVal=0;
 let bInc=true;
 let value;
+let xPos=10
+let yPos=10
+let goRight=true;
+let goDown=true;
 
 function setup() {
   createCanvas(rightSide, bottomBorder);
@@ -29,6 +33,28 @@ function draw() {
   else if (bVal==0) bInc=true;
   //Set the background in color motion
   background(rVal, gVal, bVal);  
+    //declare the bouncing ball and set it in motion
+      fill(255);
+  strokeWeight(2);
+  stroke(255,255,0);
+  ellipse(xPos,yPos,20,20);
+  // NOTE: remember that incrementation will take place by integers, if not evenly divisible by the increment
+  // the object may not exhibit the expected behavior and == wil not work as the comparison operator.
+  //use >= or <= instead
+ //define x variable behavior
+  if (xPos<width-10 && goRight==true) xPos+= 7;
+  else if(xPos>= width-10) goRight=false;
+  if (xPos > 10 && goRight!=true) xPos-=7;
+  else if (xPos==10) goRight=true;
+  //define y variable behavior
+    if (yPos<height-10 && goDown==true) yPos+= 5;
+  else if(yPos>= height-10) {
+    goDown=false;
+  }
+  if (yPos > 10 && goDown!=true) yPos-=5;
+  else if (yPos==10) goDown=true;
+    
+    //Now for the followbox
   //Constrain the possible movement of the mouse
   let mouseXConst=constrain(mouseX, leftSide+11, rightSide-11);
   let mouseYConst=constrain(mouseY, topBorder+11, bottomBorder-11);
